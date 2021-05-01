7edd2b57fedf5db29e15b67e1a47428daf72894f
Danilo <danilo181201@gmail.com>
2021-05-01 13:16:50 -0300

    Variáveis de Ambiente

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const app = require('express')();
 
 app.get('/',(req,res) => res.send({"Ola":"Mundo"}))
 
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT)
