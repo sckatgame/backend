837b605528d55d05697e14d11ff655890f2353c8
Danilo <danilo181201@gmail.com>
2021-05-01 21:48:10 -0300

    Validação de Rotas

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const {errors} = require('celebrate');
 const routes = require('./src/routes');
 
 const app = express();
 
+app.use(errors());
+app.use(cors());
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,84 @@
 const routes = require('express').Router();
+const {celebrate,Joi, Segments} = require('celebrate');
 const UserController = require('./controllers/UserController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
-routes.get('/user',UserController.index);
-routes.post('/user',UserController.create);
-routes.post('/validate',UserController.validateEmail);
-routes.post('/password',UserController.validatePassword);
-routes.put('/user',UserController.update);
+routes.get(
+    '/user',
+    celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            email:Joi.string().required().email(),
+            password:Joi.string().required()
+        })
+    }),
+    UserController.index
+);
 
-routes.get('/profile',ProfileController.index);
-routes.post('/session',SessionController.create);
+routes.post(
+    '/user',
+    celebrate({
+        [Segments.BODY]:Joi.object().keys({
+            name:Joi.string().required(),
+            email:Joi.string().required().email(),
+            password:Joi.string().required(),
+        })
+    }),
+    UserController.create
+);
+
+routes.post(
+    '/validate',
+    celebrate({
+        [Segments.BODY]:Joi.object().keys({
+            code:Joi.string().required(),
+            email:Joi.string().required().email()
+        })
+    }),
+    UserController.validateEmail
+);
+
+routes.post(
+    '/password',
+    celebrate({
+        [Segments.BODY]:Joi.object().keys({
+            email:Joi.string().required().email()
+        })
+    }),
+    UserController.validatePassword
+);
+
+routes.put(
+    '/user',
+    celebrate({
+        [Segments.BODY]:Joi.object().keys({
+            name:Joi.string().required(),
+            password:Joi.string().required(),
+            authorization:Joi.string().required()
+        })
+    }),
+    UserController.update
+);
+
+routes.get(
+    '/profile',
+    celebrate({
+        [Segments.HEADERS]:Joi.object().keys({
+            authorization:Joi.string().required()
+        }).unknown()
+    }),
+    ProfileController.index
+);
+
+routes.post(
+    '/session',
+    celebrate({
+        [Segments.BODY]:Joi.object().keys({
+            email:Joi.string().required().email(),
+            password:Joi.string().required()
+        })
+    }),
+    SessionController.create
+);
 
 module.exports = routes;
