c024f0e675af4f11e0fb291bd5000c4e8e3cffdf
Danilo <danilo181201@gmail.com>
2021-05-08 12:40:32 -0300

    Revert "Fixed Validação de código"
    This reverts commit 72d4316371c0a1880c56fa65580f1c29a83e44c3.

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -6,20 +6,13 @@ module.exports = {
 
         const user = await connection('user')
         .where({email,password})
-        .select('authorization','code')
+        .select('authorization')
         .first();
 
         if(!user){
             return res.status(400).json({error:"Usuário não encontrado"})
         }
 
-        if(user.code != 'validate'){
-            await connection('user').where('email',email).delete();
-            return res.status(400).json({error:"Usuário não encontrado"})   
-        }
-
-        const {authorization} = user;
-
-        return res.json({authorization})
+        return res.json(user)
     }
 }
