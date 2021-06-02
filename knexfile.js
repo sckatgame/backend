61ca7e42a6dcd50294d928401925f8998e9fdf5c
Danilo <danilo181201@gmail.com>
2021-06-02 20:24:34 -0300

    Fixed

--- knexfile.js
+++ knexfile.js
@@ -28,21 +28,15 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
-
+  
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './src/database/db.sqlite'
     },
-    pool: {
-      min: 2,
-      max: 10
+    migrations:{
+      directory: './src/database/migrations'
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+    useNullAsDefault: true
+  },
 };
