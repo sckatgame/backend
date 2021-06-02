b94862937c0d35e4204fcb35d6b79add6a9f40fe
Danilo <danilo181201@gmail.com>
2021-06-02 16:36:12 -0300

    Funções do jogo

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const {errors} = require('celebrate');
 const routes = require('./src/routes');
 const { init,disconnect } = require('./src/game/controllers/UserController');
+const { index, dropUser, updateData, gameOver } = require('./src/game/controllers/RoomsController');
+const { private, validateRoom } = require('./src/game/controllers/PrivatesRoomsControllers');
 
 const app = express();
 
@@ -26,7 +28,38 @@ io.on('connection',socket =>{
     socket.on('disconnect', async () =>{
         const data = await disconnect(socket.id)
         socket.broadcast.emit('update',data)
+
+        const room = dropUser(socket.id);
+        io.in(room).emit('over');
     })
+
+    socket.on('start', (name,scorre) =>{
+        const user = index(socket.id,name,scorre);
+        socket.join(user.room);
+        io.in(user.room).emit('data', user)
+    })
+    
+    socket.on('updateRoom', async (code) =>{
+        const data = await updateData(socket.id,code);
+        io.in(data.room).emit('updateRoom', data);
+    })
+
+    socket.on('over', async () =>{
+        const data = await gameOver(socket.id);
+        io.in(data.room).emit('over', data);
+    })
+
+    socket.on('private', (name,scorre,codeRoom) =>{
+        const user = private(socket.id,name,scorre,codeRoom);
+        socket.join(user.room);
+        io.in(user.room).emit('privateData', user);
+    })
+
+    socket.on('seachRoom', (codeRoom) =>{
+        const resolve = validateRoom(codeRoom);
+        socket.emit('resolve',resolve);
+    })
+
 })
 
 http.listen(process.env.PORT);
